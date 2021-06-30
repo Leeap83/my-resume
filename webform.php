9dd1dbb44ae2d20cba0172a6b28fee9b20a68ddd
Leeap83 <leeap83@gmail.com>
2021-06-30 18:50:34 +0000

    added freeform.io to forms

--- webform.php
+++ webform.php
@@ -1,29 +1,29 @@
-<?php
-    $message_sent = false;
-    if(isset($_POST['email']) && $_POST['email'] != ''){
+<!-- 
+    // $message_sent = false;
+    // if(isset($_POST['email']) && $_POST['email'] != ''){
         
-        if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
-            // Submit the Form
-            $userName = $_POST['name'];
-            $userEmail = $_POST['email'];
-            $messageSubject = $_POST['subject'];
-            $messgae = $_POST['message'];
+    //     if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+    //         // Submit the Form
+    //         $userName = $_POST['name'];
+    //         $userEmail = $_POST['email'];
+    //         $messageSubject = $_POST['subject'];
+    //         $messgae = $_POST['message'];
         
-            $to = "leeap83@gmail.com";
-            $body = "";
+    //         $to = "leeap83@gmail.com";
+    //         $body = "";
         
-            $body .= "From: ".$userName. "\r\n";
-            $body .= "Email: ".$userEmail. "\r\n";
-            $body .= "Message: ".$messgae. "\r\n";
+    //         $body .= "From: ".$userName. "\r\n";
+    //         $body .= "Email: ".$userEmail. "\r\n";
+    //         $body .= "Message: ".$messgae. "\r\n";
         
-            mail($to,$messageSubject,$body);
+    //         mail($to,$messageSubject,$body);
             
-            $message_sent = true;
-        }
+    //         $message_sent = true;
+    //     }
         
 
-    }
+    // }
 
 
 
-?>
+?> -->
